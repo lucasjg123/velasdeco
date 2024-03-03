3e58f8e64fc6fdef6eb7ec3460fa0d22835970d8
Lucas <lucas9godoy@gmail.com>
2024-03-03 14:08:18 -0300

    subiendo archivos

---
+++ carrucel.php
@@ -0,0 +1,17 @@
+<?php
+
+$cant_img = 6; // que las imagenes esten enumeradas y SEAN DEL MISMO TIPO .JPEG
+$src; // varusada en el for para almacenar temporalmente la direc de las imagenes
+
+for ($i = 1; $i < $cant_img+1; $i++) { 
+    $src = "img/carrusel/" + $i + ".jpeg"; ?>
+
+    <div class="carousel-item <?php if($i == 1) echo 'active';?>">
+        <img src="<?php echo $src; ?>" class="d-block w-100" alt="..." />
+    </div>
+<?php } ?>
+
+
+
+
+
---
+++ form.js
@@ -0,0 +1,19 @@
+// esto lo quito ya q voy a validar con php xq es un form
+
+/*let btn = document.getElementById("boton");
+let msjError = document.querySelector(".msj_error");
+let textarea = document.getElementById("textarea");
+
+textarea.addEventListener("focus", () => {
+  msjError.classList.add("show");
+});
+
+textarea.addEventListener("blur", () => {
+  msjError.classList.remove("show");
+});
+
+textarea.addEventListener("input", () => {
+  textarea.value.trim().length >= 5
+    ? (btn.disabled = false)
+    : (btn.disabled = true);
+});*/
