c6401b2cf38754efd4eedc167cb12357d987e170
Lucas <lucas9godoy@gmail.com>
2024-03-23 17:16:11 -0300

    carrucel.php: Ahora titne una funcion

--- php/carrucel.php
+++ php/carrucel.php
@@ -1,15 +1,17 @@
 <?php
+function CargarImagenes(){
+    $cant_img = 6; // que las imagenes esten enumeradas y SEAN DEL MISMO TIPO .JPEG
+    $src; // varusada en el for para almacenar temporalmente la direc de las imagenes
 
-$cant_img = 6; // que las imagenes esten enumeradas y SEAN DEL MISMO TIPO .JPEG
-$src; // varusada en el for para almacenar temporalmente la direc de las imagenes
+    for ($i = 1; $i < $cant_img+1; $i++) { 
+        $src = "img/carrusel/" + $i + ".jpeg"; ?>
 
-for ($i = 1; $i < $cant_img+1; $i++) { 
-    $src = "img/carrusel/" + $i + ".jpeg"; ?>
+        <div class="carousel-item h-100 <?php if($i == 1) echo 'active';?>">
+            <img src="<?php echo $src; ?>" class="d-block w-100 h-100" alt="..." />
+        </div>
+    <?php } 
+ } ?>
 
-    <div class="carousel-item h-100 <?php if($i == 1) echo 'active';?>">
-        <img src="<?php echo $src; ?>" class="d-block w-100 h-100" alt="..." />
-    </div>
-<?php } ?>
 
 
 
