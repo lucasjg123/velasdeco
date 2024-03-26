46e4ae2d1a9958eee0ba15acc842c20602fa7a14
Lucas <lucas9godoy@gmail.com>
2024-03-26 18:41:33 -0300

    Agregamos bootstrap.css con el new breakpoint  xxxl

--- index.php
+++ index.php
@@ -61,10 +61,8 @@ mysqli_close($MiConexion);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      href="css/bootstrap.css"
       rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous"
     />
     <link
       rel="stylesheet"
