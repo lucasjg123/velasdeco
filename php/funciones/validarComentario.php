578dbb9b0649e0364aab5971a4f2abedf5d5bf98
Lucas <lucas9godoy@gmail.com>
2024-03-31 14:14:32 -0300

    php: mejoras de codigo

--- php/funciones/validarComentario.php
+++ php/funciones/validarComentario.php
@@ -1,10 +1,12 @@
 <?php
-function validar(){
-    $vMensaje='';
-    if(trim(strlen($_POST['comentario'])) < 5){
-        $vMensaje.="Debes ingresar un minimo de 5 caracteres!";
+function validar() {
+    $vMensaje = '';
+    $comentario = trim($_POST['comentario']); // Elimina espacios en blanco al principio y al final
+
+    if (strlen($comentario) < 5) {
+        $vMensaje = "Debes ingresar al menos 5 caracteres.";
     }
-    
+
     return $vMensaje;
 }
 
